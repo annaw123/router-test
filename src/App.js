ecf04a850041042bc07441a931cf5331e384148d
annaw123 <53934079+annaw123@users.noreply.github.com>
2022-01-20 17:00:52 +0000

    Co-authored-by: Hjwake <Hjwake@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
+// import Navbar from './Components/Navbar';
+// import Header from './Components/Header';
+// import Tutorial from './Components/Tutorial';
+// import Footer from "./Components/Footer";
+
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div>
+//       <Navbar />
+//       <Header />
+//       <Tutorial />
+//       <Footer />
+//     </div>
+//   );
+// }
+
 import { Outlet, Link } from "react-router-dom";
+import "./index.css";
 
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper</h1>
+      <h1>Mission Encodeable</h1>
+      <p>It won't be this ugly for long, we promise!</p>
       <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem"
         }}
       >
-        <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link> |{" "}
         <Link to="/courses">Courses</Link>
       </nav>
       <Outlet />
